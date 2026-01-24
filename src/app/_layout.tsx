068c0059cf6971c992607e3184359e0c8b8be5fb
Wallyson Galvão <wallyson.galvao@gmail.com>
2026-01-24 09:14:06 -0300

    feat: add drizzle studio config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,7 @@ import '../configs';
 import { useEffect } from 'react';
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { usePerformanceMonitorDevTools } from '@rozenite/performance-monitor-plugin';
 import * as Sentry from '@sentry/react-native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -45,6 +46,7 @@ function RootLayout() {
   const theme = useTheme();
   const isDark = theme === 'dark';
   const { getAlarmById } = useAlarms();
+  usePerformanceMonitorDevTools();
   const { initialize: initializeSubscription } = useSubscriptionStore();
 
   const [fontsLoaded] = useFonts({
